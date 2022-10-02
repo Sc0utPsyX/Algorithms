323eaea5efb8a8a39f5d0c25daed377d4b10d884
sc0ut <sc0utpsyx@gmail.com>
2022-10-02 21:57:33 +0300

    lesson_6

---
+++ src/main/java/lesson_6/Tree.java
@@ -0,0 +1,18 @@
+package lesson_6;
+import java.util.function.Consumer;
+
+public interface Tree<T> {
+
+    void add(T item);
+
+    boolean contains(T item);
+
+    void remove(T item);
+
+    void dfs(Consumer<T> visitor);
+
+    void bfs(Consumer<T> visitor);
+
+    boolean isBalanced();
+
+}
